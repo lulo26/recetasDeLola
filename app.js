2554df2700c43eb50269bd398b08ef7870194c38
lulo <lopez.gallego2611@gmail.com>
2024-09-10 03:42:28 -0500

    conexion al local storage está fallando arreglar js

--- app.js
+++ app.js
@@ -1,26 +1,63 @@
+//variants
 let formTitle = document.getElementById("formTitle");
 let formDesc = document.getElementById("formDesc");
 let formCost = document.getElementById("formCost");
 let formPrice = document.getElementById("formPrice");
+let formImg = document.getElementById("formImg");
+let saveData = document.getElementById("saveData");
+let containerCards = document.getElementById("containerCards");
 
+const getData = window.localStorage;
+
+//Modal initialize
 document.addEventListener("DOMContentLoaded", function () {
   var elems = document.querySelectorAll(".modal");
   var instances = M.Modal.init(elems);
 });
 
-console.log(localStorage.length);
+saveData.addEventListener("click", () => {
+  let newObject = {
+    tituloReceta: formTitle.value,
+    descripcion: formDesc.value,
+    costo: formCost.value,
+    precio: formPrice.value,
+    imagen: formImg.value,
+  };
 
-let stringJSON = `
-{
-    "tituloReceta": "algo",
-    "descripcion": "",
-    "costo": 23,
-    "precio":456
+  getData.setItem(JSON.stringify(newObject));
+  PrintInfo();
+});
+
+function PrintInfo() {
+  containerCards.innerHTML = "";
+  let getKeys = Object.keys(getData);
+  getKeys.map((thisKey) => {
+    let recipe = JSON.parse(getData.getItem(thisKey));
+    let infoHere = `<div class="col s12 m7">
+        <div class="card horizontal">
+          <div class="card-image">
+            <img src="${recipe.imagen}" />
+          </div>
+          <div class="card-stacked">
+            <div class="card-content">
+              <!-- <p> -->
+              <b>${recipe.tituloReceta}</b>
+              <ul>
+                <li>${recipe.descripcion}</li>
+                <li>costo de fabricacion: ${recipe.costo}</li>
+                <li>precio sugerido de venta: ${recipe.precio}</li>
+              </ul>
+              <!-- </p> -->
+            </div>
+          </div>
+        </div>
+      </div>`;
+    containerCards.innerHTML += infoHere;
+  });
 }
-`;
 
 //localStorage.setItem(localStorage.length + 1, stringJSON);
-
+/* 
 let stirngObtenido = localStorage.getItem(3);
 
 let objetoConvertido = JSON.parse(stirngObtenido);
@@ -28,3 +65,4 @@ let objetoConvertido = JSON.parse(stirngObtenido);
 console.log(stirngObtenido);
 console.log();
 console.log(objetoConvertido.tituloReceta);
+ */
